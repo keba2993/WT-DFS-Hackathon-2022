79b1e7d9480a5236fa229da688c95a00f137ed3a
Gabrielle Stein <gabbystein125@gmail.com>
2022-07-28 16:37:14 -0400

    Get loyalty points and id from url

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -74,3 +74,16 @@ function resetBoard() {
 })();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const loyaltyId = urlParams.get('loyaltyId');
+const loyaltyPoints = urlParams.get('loyaltyPoints');
+var loyalty1 = document.getElementById("loyaltyId");
+var loyalty2 = document.getElementById("loyaltyPoints");
+if (lolyaltyId != null){
+loyalty1.append(loyaltyId);
+}
+if (lolyaltyPoints!= null){
+loyalty2.append(loyaltyPoints);
+}
